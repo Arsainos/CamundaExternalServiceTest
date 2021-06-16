7cf1371a76a21e7d88fa2f5798d168439d3fbffe
Белявцев Артём <Arsainos@gmail.com>
2021-06-16 14:29:38 +0300

    Добавьте файлы проекта.

---
+++ CamundaExternalServiceTest/Handlers/ExternalTestHandler.cs
@@ -0,0 +1,27 @@
+﻿using Camunda.Worker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CamundaExternalServiceTest.Handlers
+{
+    [HandlerTopics("ExternalTestTopic", LockDuration = 10000)]
+    public class ExternalTestHandler : IExternalTaskHandler
+    {
+        public async Task<IExecutionResult> HandleAsync(ExternalTask externalTask, CancellationToken cancellationToken)
+        {
+            await Task.Delay(2000);
+
+            return new CompleteResult
+            {
+                Variables = new Dictionary<string, Variable>
+                {
+                    ["MESSAGE"] = Variable.String("Hello World!")
+                }
+            };
+           
+        }
+    }
+}
